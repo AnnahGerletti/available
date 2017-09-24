731ed622db78743d785157d3d15eaa66859c55b8
AnnahGerletti <agerletti@gmail.com>
2017-09-24 14:46:08 +1300

    routing to admin page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom'
+
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Admin from './components/Admin';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <Router>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route exact path='/admin' component={Admin} />
+    </div>  
+  </Router>
+), document.getElementById('root'))
+
 registerServiceWorker();
